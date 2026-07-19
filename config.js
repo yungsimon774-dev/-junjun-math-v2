dc74102cb6a875dde4f3fc12030ac39ea72bafe8
yungsimon774-dev <yungsimon774@gmail.com>
2026-07-19 13:57:20 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.MATH_CONFIG={"appName":"峻峻数学大冒险","version":"V11.0 · 长期稳定版","parentPin":"2026","speechRate":0.9,"speechPitch":1};
+window.MATH_CONFIG={"appName":"峻峻数学大冒险","version":"V11长期版 · Build 002 · 等量代换完整课程","parentPin":"2026","speechRate":0.9,"speechPitch":1};
